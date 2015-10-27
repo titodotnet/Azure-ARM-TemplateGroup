452dc7585062c683aa29f0def5bc18eaa03c968c
Tito Dakshinamoorthy <titodakshinamoorthy@hotmail.com>
2015-10-27 20:22:49 +0530

    SQL server link template created

---
+++ ARMTemplateLibrary/Scripts/Common/Parameter.psm1
@@ -0,0 +1,9 @@
+#
+# Parameter.psm1
+#
+$Global:Environment="test1"
+$Global:SubscriptionId=""
+
+$Global:Location = "West Europe"
+$Global:DBResourceGroupName = "titotestrg$Environment"
+$Global:SqlServerName = "titotestsqlserver$Environment"
